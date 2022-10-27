f121f819f973b21773d864d7a2c9a1dd3c157417
MarkHenderson02 <114543984+MarkHenderson02@users.noreply.github.com>
2022-10-27 10:42:40 +0800

    1 Dimension

---
+++ Form1.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LV1 = New System.Windows.Forms.ListView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnTambah = New System.Windows.Forms.Button()
+        Me.txtNim = New System.Windows.Forms.TextBox()
+        Me.txtNama = New System.Windows.Forms.TextBox()
+        Me.cbProdi = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'LV1
+        '
+        Me.LV1.HideSelection = False
+        Me.LV1.Location = New System.Drawing.Point(23, 230)
+        Me.LV1.Name = "LV1"
+        Me.LV1.Size = New System.Drawing.Size(384, 202)
+        Me.LV1.TabIndex = 0
+        Me.LV1.UseCompatibleStateImageBehavior = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(78, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(32, 16)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "NIM"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(78, 75)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 16)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Nama "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(78, 120)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(40, 16)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Prodi"
+        '
+        'btnTambah
+        '
+        Me.btnTambah.Location = New System.Drawing.Point(166, 170)
+        Me.btnTambah.Name = "btnTambah"
+        Me.btnTambah.Size = New System.Drawing.Size(98, 35)
+        Me.btnTambah.TabIndex = 4
+        Me.btnTambah.Text = "Tambah"
+        Me.btnTambah.UseVisualStyleBackColor = True
+        '
+        'txtNim
+        '
+        Me.txtNim.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNim.Location = New System.Drawing.Point(155, 27)
+        Me.txtNim.Name = "txtNim"
+        Me.txtNim.Size = New System.Drawing.Size(109, 22)
+        Me.txtNim.TabIndex = 5
+        '
+        'txtNama
+        '
+        Me.txtNama.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNama.Location = New System.Drawing.Point(155, 72)
+        Me.txtNama.Name = "txtNama"
+        Me.txtNama.Size = New System.Drawing.Size(197, 22)
+        Me.txtNama.TabIndex = 6
+        '
+        'cbProdi
+        '
+        Me.cbProdi.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cbProdi.FormattingEnabled = True
+        Me.cbProdi.Items.AddRange(New Object() {"Tehnik Informatika", "Sistem Informasi"})
+        Me.cbProdi.Location = New System.Drawing.Point(155, 117)
+        Me.cbProdi.Name = "cbProdi"
+        Me.cbProdi.Size = New System.Drawing.Size(162, 24)
+        Me.cbProdi.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(431, 445)
+        Me.Controls.Add(Me.cbProdi)
+        Me.Controls.Add(Me.txtNama)
+        Me.Controls.Add(Me.txtNim)
+        Me.Controls.Add(Me.btnTambah)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.LV1)
+        Me.Name = "Form1"
+        Me.Text = "1 Dimension"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LV1 As ListView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents txtNim As TextBox
+    Friend WithEvents txtNama As TextBox
+    Friend WithEvents cbProdi As ComboBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LV1.View = View.Details
+        LV1.GridLines = True
+        LV1.Columns.Add("NIM", 120)
+        LV1.Columns.Add("Nama", 120)
+        LV1.Columns.Add("Prodi", 120)
+
+        txtNim.Text = 1
+        txtNama.Focus()
+    End Sub
+
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+        Dim Arr(2) As String
+        Arr(0) = txtNim.Text
+        Arr(1) = txtNama.Text
+        Arr(2) = cbProdi.Text
+
+        Dim listitem As ListViewItem
+        listitem = New ListViewItem
+        listitem = LV1.Items.Add(Arr(0))
+        listitem.SubItems.Add(Arr(1))
+        listitem.SubItems.Add(Arr(2))
+
+        txtNim.Text = txtNim.Text + 1
+        txtNama.Text = ""
+        cbProdi.Text = ""
+        txtNama.Focus()
+    End Sub
+End Class
